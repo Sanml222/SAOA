4b2887bb6f1889bb4f7715bb94fee995da4b5389
Santiago Munoz <Santiagoml222@gmail.com>
2023-11-16 15:47:17 -0500

    tests vuelos operados y cancelados

--- src/test/Entidades/AerolineaTest.java
+++ src/test/Entidades/AerolineaTest.java
@@ -17,7 +17,12 @@ import Entidades.Vuelo;
     testVuelosProgramadosListaVacia()  | 0 vuelos Programados   | 0
     testVuelosProgramados()            | 2 vuelos Programados   | 2
     testVuelosProgramadosErroneos()    | 2 vuelos Programados   | !2
-
+    testVuelosOperadosListaVacia()     | 0 vuelos Operados      | 0
+    testVuelosOperados()               | 1 vuelos Operados      | 1
+    testVuelosOperadosErroneos()       | 1 vuelos Operados      | !1
+    testVuelosCanceladosListaVacia()   | 0 vuelos Operados      | 0
+    testVuelosCancelados()             | 3 vuelos Operados      | 3
+    testVuelosCanceladosErroneos()     | 3 vuelos Operados      | !3
 */
 
 public class AerolineaTest {
@@ -121,6 +126,117 @@ public class AerolineaTest {
         assertNotEquals(9, programados.size());
     }
 
+    @Test
+    public void testVuelosOperadosListaVacia() {
 
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Programado", null);
+
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+
+        List<Vuelo> operados = aerolinea1.vuelosOperados();
+
+        assertEquals(0, operados.size());
+    }
+
+    @Test
+    public void testVuelosOperados() {
+
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Programado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "En Aire", null);
+        Vuelo vuelo3 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Aterrizado", null);
+       
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+        aerolinea1.getVuelos().add(vuelo3);
+
+        List<Vuelo> operados = aerolinea1.vuelosOperados();
+
+        assertEquals(1, operados.size());
+    }
+
+    @Test
+    public void testVuelosOperadosErroneos() {
+
+
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Programado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "En Aire", null);
+        Vuelo vuelo3 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Operados", null);
+       
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+        aerolinea1.getVuelos().add(vuelo3);
+
+        List<Vuelo> Operados = aerolinea1.vuelosOperados();
+
+        assertNotEquals(7, Operados.size());
+    }
+
+    @Test
+    public void testVuelosCanceladosListaVacia() {
+
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Aterrizado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Programado", null);
+
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+
+        List<Vuelo> cancelados = aerolinea1.vuelosCancelados();
+
+        assertEquals(0, cancelados.size());
+    }
+
+        @Test
+    public void testVuelosCanceladoss() {
+
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+        Vuelo vuelo3 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+       
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+        aerolinea1.getVuelos().add(vuelo3);
+
+        List<Vuelo> cancelados = aerolinea1.vuelosCancelados();
+
+        assertEquals(3, cancelados.size());
+    }
+
+    @Test
+    public void testVuelosCanceladosErroneos() {
+
+
+        Aerolinea aerolinea1 = new Aerolinea("Avianca");
+
+        Vuelo vuelo1 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+        Vuelo vuelo2 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+        Vuelo vuelo3 = new Vuelo(null, aerolinea1, null, null, null, null, null, null, null, null, null, null, 0, "Cancelado", null);
+       
+        // Simulamos que existen dos vuelos creados
+        aerolinea1.getVuelos().add(vuelo1);
+        aerolinea1.getVuelos().add(vuelo2);
+        aerolinea1.getVuelos().add(vuelo3);
+
+        List<Vuelo> cancelados = aerolinea1.vuelosCancelados();
+
+        assertNotEquals(4, cancelados.size());
+    }
 }
 
+  
